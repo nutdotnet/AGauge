8ebb6075ca633a2e733e0ce09da01e19d57ede67
gbakeman <gbakeman@live.com>
2022-12-04 13:40:48 -0800

    Cleaning usings

--- AGauge/AGauge.cs
+++ AGauge/AGauge.cs
@@ -23,16 +23,10 @@
 //
 // -----------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using System.Drawing.Drawing2D;
 using System.Diagnostics;
-using System.Collections;
 using System.ComponentModel.Design;
 
 namespace System.Windows.Forms
