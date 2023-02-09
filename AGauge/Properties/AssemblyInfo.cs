f5eaeba5c3e1c686f974e5be3c5d19c4da2d7965
gbakeman <gbakeman@live.com>
2023-02-09 17:55:47 -0500

    Updating metadata
    Rearranging the .nuspec file, and deleting a redundant solution file.

--- AGauge/Properties/AssemblyInfo.cs
+++ AGauge/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AGauge")]
-[assembly: AssemblyDescription("AGauge Compoent by A.J.Bauer")]
+[assembly: AssemblyTitle("AGauge Classic")]
+[assembly: AssemblyDescription("Customizable WinForms gauge control")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Code Art Engineering")]
+[assembly: AssemblyCompany("NUTDotNet Organization")]
 [assembly: AssemblyProduct("AGauge")]
-[assembly: AssemblyCopyright("Copyright © CodeArtEng 2012")]
+[assembly: AssemblyCopyright("Copyright © NUTDotNet 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.4.6")]
-[assembly: AssemblyFileVersion("2.0.4.6")]
+[assembly: AssemblyVersion("2.0.5")]
+[assembly: AssemblyFileVersion("2.0.5")]
